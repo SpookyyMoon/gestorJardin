cedefc67118c7249d6e22021f526b3969fe535f5
SpookyLuna <spookyluna666@gmail.com>
2025-06-11 18:52:29 +0200

    Post del formulario de creación de plantas, creación de plantas, selector en tipo de planta

--- public/script/script.js
+++ public/script/script.js
@@ -1,16 +1,23 @@
 $(document).ready(function () {
-
   const plantaCrear = `<div class="plantaTarjeta">
+                          <form method="post" action="/crearPlanta">
                             <p>Nombre de la planta: 
-                                <input>
+                                <input name="nombrePlanta" type="text" placeholder="Nombre de la planta [EJ: Planta1]">
                             </p>
                             <p>Tipo de la planta: 
-                                <input>
+                                  <select name="tipoPlanta">
+                                    <option value="1">Alocasia</option>
+                                    <option value="2">Costilla de Adán</option>
+                                    <option value="3">Aloe Vera</option>
+                                    <option value="4">Lirio</option>
+                                    <option value="5">Lengua de suegra</option>
+                                  </select>
                             </p>
                             <p>Frencuencia de riego de la planta: 
-                                <input>
+                                <input name="frecuenciaRiego" type="number" placeholder="Frencuencia en días [EJ: 5]">
                             </p>
                             <button>Crear Planta</button>
+                          </form>
                         </div>`;
 
   $(".plantaAdd").click(function () {
@@ -18,6 +25,8 @@ $(document).ready(function () {
     $("#contenido").append(plantaCrear);
   });
 
-  
-
+  $(document).on("click", "button", function () {
+  });
 });
+
+//Plantas: Alocasia, Costilla de Adán, Aloe Vera, Lirio, Lengua de suegra
--- server.js
+++ server.js
@@ -1,42 +1,38 @@
 // Imports
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
+const mysql = require('mysql2/promise');
 
 // Middleware
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // Iniciar servidor
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log("Iniciando servidor...");
-    console.log(`Servidor iniciado en: http://localhost:${PORT}`);
+  console.log("Iniciando servidor...");
+  console.log(`Servidor iniciado en: http://localhost:${PORT}`);
 });
 
+//Gestor de la planta
+app.post("/crearPlanta", (req, res) => {
+  var nombrePlanta = req.body.nombrePlanta;
+  var frecuenciaRiego = req.body.frecuenciaRiego;
+  var tipoPlanta = req.body.tipoPlanta;
 
-class Planta {
-  constructor(nombre, tipo, frecuenciaRiego, ultimoRiego) {
-    this.nombre = nombre;
-    this.tipo = tipo;
-    this.frecuenciaRiego = frecuenciaRiego;
-    this.ultimoRiego = ultimoRiego;
-  }
+  console.log("Datos recibidos: ", {
+    nombrePlanta,
+    frecuenciaRiego,
+    tipoPlanta,
+  });
 
-  ultimoRiego(planta) {
-    return;
-  }
-
-  necesitaRiego(planta) {
-    return;
-  }
-
-  crearPlanta(nombre, tipo, frecuenciaRiego) {
-    let planta = new Planta(nombre, tipo, frecuenciaRiego, null);
-    console.log(
-      `\nSe ha añadido la planta: ${planta.nombre} de tipo: ${planta.tipo}\n`
-    );
-  }
-}
+  Planta.crearPlanta(nombrePlanta, tipoPlanta, frecuenciaRiego);
+});
 
 let conexion;
 let host = "";
@@ -59,5 +55,29 @@ async function conexionSQL() {
     console.log("\nConexión a MySQL establecida correctamente");
   } catch (error) {
     console.error("\nError al conectar a MySQL:", error);
+  }
+}
+
+class Planta {
+  constructor(nombre, tipo, frecuenciaRiego, ultimoRiego) {
+    this.nombre = nombre;
+    this.tipo = tipo;
+    this.frecuenciaRiego = frecuenciaRiego;
+    this.ultimoRiego = ultimoRiego;
+  }
+
+  static ultimoRiego(planta) {
+    return;
+  }
+
+  static necesitaRiego(planta) {
+    return;
+  }
+
+  static crearPlanta(nombre, tipo, frecuenciaRiego) {
+    let planta = new Planta(nombre, tipo, frecuenciaRiego, null);
+    console.log(
+      `\nSe ha añadido la planta: ${planta.nombre} de tipo: ${planta.tipo}\n`
+    );
   }
 }
